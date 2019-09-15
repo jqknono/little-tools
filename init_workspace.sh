d231826188d8fc6f75bca333aae3c54ca1fb7de8
jqknono <jqknono@outlook.com>
2019-09-16 00:06:00 +0800

    填数据到excel

---
+++ init_workspace.sh
@@ -0,0 +1,1 @@
+pip install pandas numexpr bottleneck BeautifulSoup4 Jinja2 PyQt5 SQLAlchemy SciPy XLsxWriter blosc fastparquet gcsfs html5lib lxml matplotlib openpyxl pandas psycopg2 pymysql pyreadstat qtpy s3fs xarray xlrd xlwt
