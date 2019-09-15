8bb7ca869e0e383b44808ec75db3027d2b10a63e
fanfan <18894012637@163.com>
2019-09-15 23:58:09 +0800

    内购金额及数量

--- id.py
+++ id.py
@@ -8,5 +8,10 @@ event_group_id_open_map2 = '5b84cc7ca40fa34b91000019'
 event_group_id_open_map3 ='5b9a2569b27b0a5cb8000054'
 event_group_id_open_event = '5b7289ddf43e4838c7000282'
 event_group_id_show_claim = '5b7289ddf43e4838c7000289'
+event_group_id_unlock_business = '5b7289ddf43e4838c7000295'
+event_group_id_buy_iap_total= '5b84cc7ca40fa34b91000015'
+
 
 event_id_show_claim = '5b7be0660237f6071568cc54'
+event_id_unlock_business= '5b7bdfa20237f6071568cc50'
+event_id_buy_iap_total = '5b9f7401c37c58067735ebc0'
