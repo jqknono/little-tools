9577b6f681ca3f1eb3103a3bb74a502964167eeb
fanfan <18894012637@163.com>
2019-09-22 14:11:00 +0800

    excel写入内购次数及金额

--- excel.py
+++ excel.py
@@ -45,7 +45,8 @@ data10 = query_open_map3_users()
 data11 = query_open_event()
 data12 = query_claim_times()
 data13 = query_iap_money()
-
+data14 = calculate_iap_nums()
+data15 = calculate_iap_money_sum()
 
 #数据计算
 value_E = data['昨天'][0] / data['昨天'][1]    # E列生命周期
@@ -56,8 +57,14 @@ value_O = data5['昨天'][1]/data['昨天'][0]     # O列视频用户覆盖率
 value_X = data6['昨天']/data['昨天'][0]        # x列人均插频次数
 value_Y = (data5['昨天'][0]+data6['昨天'])/data['昨天'][0]        # Y列人均视频+插频次数
 
-value_AA = data7['昨天']                       # AA列内购人数
+value_z = data14                             # Z列内购次数
+
+value_AA = data7['昨天']                      # AA列内购人数
+value_AB = data15                             # AB列内购金额
+value_AD = data14/data['昨天'][0]             # AD列日活人均付费次数
 value_AE = data7['昨天']/data['昨天'][0]       # AE列日活付费率
+value_AF = data15/data14                      # AF列单次付费价值
+value_AG = data15/data['昨天'][0]              # AG列日活人均付费金额
 
 value_AH = data8['昨天'][0]/data['昨天'][1]    # AH列新增用户解锁第5产业占比
 value_AI = data8['昨天'][1]/data['昨天'][1]    # AI列新增用户解锁第10产业占比
@@ -80,11 +87,11 @@ row_K = new_row-7
 # 填数据到excel
 
 assign_value(new_row, 'A',yesterday)
-assign_value(new_row, 'b','iOS')
-
+assign_value(new_row, 'B','iOS')
 assign_value(new_row, 'C',data['昨天'][1])
 assign_value(new_row, 'D',data['昨天'][0])
 assign_value(new_row, 'E',value_E)
+
 assign_value(row_I, 'I',data1['昨天']/100)
 assign_value(row_J, 'J',data2['昨天']/100)   
 assign_value(row_K, 'K',data3['昨天']/100)       
@@ -95,10 +102,13 @@ assign_value(new_row, 'O',value_O)
 
 assign_value(new_row, 'X',value_X)
 assign_value(new_row, 'Y',value_Y)
-
+assign_value(new_row, 'Z',value_z)
 assign_value(new_row, 'AA',value_AA)
+assign_value(new_row, 'AB',value_AB)
+assign_value(new_row, 'AD',value_AD)
 assign_value(new_row, 'AE',value_AE)
-
+assign_value(new_row, 'AF',value_AF)
+assign_value(new_row, 'AG',value_AG)
 assign_value(new_row, 'AH',value_AH)
 assign_value(new_row, 'AI',value_AI)
 assign_value(new_row, 'AJ',value_AJ)
