c665f4e0a14a544f5333c527abb14e84bc339fe0
jqknono <jqknono@outlook.com>
2019-11-17 22:53:18 +0800

    更新template.xlsx文件

---
+++ init_workspace_3.6.sh
@@ -0,0 +1,1 @@
+pip3.6 install hyper pandas numexpr bottleneck BeautifulSoup4 Jinja2 PyQt5 SQLAlchemy SciPy XLsxWriter blosc fastparquet gcsfs html5lib lxml matplotlib openpyxl pandas psycopg2 pymysql pyreadstat qtpy s3fs xarray xlrd xlwt
