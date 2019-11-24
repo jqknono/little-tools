27ae9ce97faf03900fce460b9a45d2d4bb3ae888
jqknono <jqknono@outlook.com>
2019-11-24 20:46:21 +0800

    适应新的友盟平台

--- functions.py
+++ functions.py
@@ -39,16 +39,9 @@ url_eventname_temlate = '%(base_url)s/%(id)s/%(report_type)s/load_table_data?sta
 
 interval = 8
 
-cookies = {'JSESSIONID': '26A64F4332F1D3C22CFB9778FAA46811',
-           'PHPSESSID': 'e25rks4rchtepldv23flhc7l82',
-           'UM_distinctid': '16d10cf0765141-056e45dadb0c15-5373e62-240000-16d10cf0766e88',
-           'isg': 'BHl5F5L7ANyeINyI5BmIkmxxiOWTLm1UkGz0TJuuxaAfIpm049KxCWJ7oG5xmgVw', 
-           'uc_session_id': 'd521e33a-ddf0-4d6d-8ed3-a1d082639bd6',
-           'um_lang': 'zh',
-           'ummo_ss': 'BAh7CUkiGXdhcmRlbi51c2VyLnVzZXIua2V5BjoGRVRbCEkiCVVzZXIGOwBGWwZvOhNCU09OOjpPYmplY3RJZAY6CkBkYXRhWxFpYmkxaVppAZppXGkSaQHzaQGTaT5pAGkKaXRJIhk4UVA3eElpcW1PaGRXdG1HUFpQawY7AFRJIhR1bXBsdXNfdWNfdG9rZW4GOwBGIj0xdVFkY1hjb3I2ZmtRXzI2bzVnMzYzd18wZTI2MGI3Yjc4Y2U0MjBkYjI5ODk4Yjk3MTk0ZjFkNUkiEF9jc3JmX3Rva2VuBjsARkkiMXdqZjBnY1EvLzYxazNTRnd6WFlOL3pTQlQ1M1Vxdk5vRXVmMTg0aGR6enM9BjsARkkiD3Nlc3Npb25faWQGOwBUSSIlMWU5NDQwZmE1NmY5Y2ZlYWVhYzUwNzg0MjllNzQyYmIGOwBG--4011cfd5b83413aedc5acc4244e0e8e6bb3d73f3',
-           'umplus_uc_loginid': 'fangfang_ren',
-           'umplus_uc_token': '1uQdcXcor6fkQ_26o5g363w_0e260b7b78ce420db29898b97194f1d5'
-           }
+related_Id = "5b7139be8f4a9d7dea000051"
+
+cookies_str = 'umplus_uc_loginid=fangfang_ren; UM_distinctid=16d10cf0765141-056e45dadb0c15-5373e62-240000-16d10cf0766e88; um_lang=zh; cn_1276392090_dplus=1%5B%7B%7D%2C0%2C1567949770%2C0%2C1567949770%2Cnull%2C%2216d10cf0765141-056e45dadb0c15-5373e62-240000-16d10cf0766e88%22%2C%221567940381%22%2C%22https%3A%2F%2Fpassport.umeng.com%2Freg-redirect.html%3FredirectUrl%3Dhttps%253A%252F%252Fweb.umeng.com%252Fmain.php%253Fc%253Dsite%2526a%253Dshow%2526from%253Dlogin%22%2C%22passport.umeng.com%22%5D; cna=6axpFZjaSlYCATuvJCB7x6Bc; uc_session_id=d521e33a-ddf0-4d6d-8ed3-a1d082639bd6; umplus_uc_token=1ZsL_GyVMO09c7WqHRCzffA_c1fa63173fb6450da72b09f72173a34f; cn_1258498910_dplus=1%5B%7B%7D%2C0%2C1574562491%2C0%2C1574562491%2C%22%24direct%22%2C%2216d10cf0765141-056e45dadb0c15-5373e62-240000-16d10cf0766e88%22%2C%221569051311%22%2C%22https%3A%2F%2Fmobile.umeng.com%2Fhome%2Fcheck_login%3Furl%3Dhttps%253A%252F%252Fpassport.umeng.com%252Flogin%253FappId%253Dumeng%22%2C%22mobile.umeng.com%22%5D; isg=BKSkFmh5pc7Q6tEvgfINnQH6daJWlcjLL6uAhL7GY2_0aUcz5kyYNZjILYlUsQD_; frame=; ummo_ss=BAh7CEkiGXdhcmRlbi51c2VyLnVzZXIua2V5BjoGRVRbCEkiCVVzZXIGOwBGWwZvOhNCU09OOjpPYmplY3RJZAY6CkBkYXRhWxFpYmkxaVppAZppXGkSaQHzaQGTaT5pAGkKaXRJIhk4UVA3eElpcW1PaGRXdG1HUFpQawY7AFRJIhR1bXBsdXNfdWNfdG9rZW4GOwBGIj0xWnNMX0d5Vk1PMDljN1dxSFJDemZmQV9jMWZhNjMxNzNmYjY0NTBkYTcyYjA5ZjcyMTczYTM0ZkkiD3Nlc3Npb25faWQGOwBUSSIlNTZlMDAwZTNlZmI4Y2I0MzhkN2MxMDgxOTc0MWEzYjIGOwBG--3c163cc54dceafece46b51a7625222f27f159f75; cn_1259864772_dplus=1%5B%7B%22%E6%98%AF%E5%90%A6%E7%99%BB%E5%BD%95%22%3Atrue%2C%22UserID%22%3A%22fangfang_ren%22%2C%22Uapp_appkey%22%3A%225b7139be8f4a9d7dea000051%22%2C%22Uapp_platform%22%3A%22iphone%22%7D%2C0%2C1574597949%2C0%2C1574597949%2C%22%24direct%22%2C%2216d10cf0765141-056e45dadb0c15-5373e62-240000-16d10cf0766e88%22%2C%221567941798%22%2C%22%24direct%22%2C%22%24direct%22%5D; cn_1273967994_dplus=1%5B%7B%7D%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%2216d10cf0765141-056e45dadb0c15-5373e62-240000-16d10cf0766e88%22%2C%221567945446%22%2C%22https%3A%2F%2Fmobile.umeng.com%2Fplatform%2Fapps%2Flist%22%2C%22mobile.umeng.com%22%5D; CNZZDATA1259864772=1505950409-1567941798-%7C1574596551; JSESSIONID=2D8F832024A037F1EDB436FFC7DA8CB8'
 
 query_xi_back_gun_connect_sid = unquote(
     's%3Ae_O5RJ8-rXFpEW91Ua7eKVSABBJEanws.w3NAuc1%2BI6Ni%2FDg%2BzuQ%2BZJ3zWwqdAytz0zYkzVkgRyM')
@@ -91,7 +84,7 @@ def stat_platform_ios():
 
     event_id_buy_iap_total = '5b9f7401c37c58067735ebc0'
     event_id_show_claim = '5b7be0660237f6071568cc54'
-    event_id_unlock_business= '5b7bdfa20237f6071568cc50'
+    event_id_unlock_business = '5b7bdfa20237f6071568cc50'
     event_id_ad_play_action = '5b7a8b163665ca0789e82188'
     event_id_ad_play_event = '5b74f0b25a16bc074d5f2633'
     event_id_ad_play_map2 = '5b9f0bd60237f606c0aca277'
@@ -109,7 +102,6 @@ def stat_platform_android():
     global g_bundle_id
     global g_platform
 
-    
     global event_group_id_ad_interstitial_from
     global event_group_id_watch_video
     global event_group_id_buy_iap_total
@@ -127,22 +119,22 @@ def stat_platform_android():
     global event_id_ad_play_map2
     global event_id_ad_play_map3
 
-    event_group_id_ad_interstitial_from = '5b3de4bca40fa3572900007d' # ok
-    event_group_id_watch_video = '5b3de4bca40fa35729000065' # ok
-    event_group_id_buy_iap_total = '5b7e21c5f43e48055e000042' # ok
-    event_group_id_open_map2 = '5b7e2198b27b0a7a4200001b' # ok
-    event_group_id_open_map3 = '5b9a23a1f29d983795000100' # ok
-    event_group_id_open_event = '5b642551f29d98672d000163' # ok
-    event_group_id_show_claim = '5b3eee9cf29d9833db0001db' # ok
-    event_group_id_unlock_business = '5b3de4bca40fa35729000078' # ok 
+    event_group_id_ad_interstitial_from = '5b3de4bca40fa3572900007d'  # ok
+    event_group_id_watch_video = '5b3de4bca40fa35729000065'  # ok
+    event_group_id_buy_iap_total = '5b7e21c5f43e48055e000042'  # ok
+    event_group_id_open_map2 = '5b7e2198b27b0a7a4200001b'  # ok
+    event_group_id_open_map3 = '5b9a23a1f29d983795000100'  # ok
+    event_group_id_open_event = '5b642551f29d98672d000163'  # ok
+    event_group_id_show_claim = '5b3eee9cf29d9833db0001db'  # ok
+    event_group_id_unlock_business = '5b3de4bca40fa35729000078'  # ok
 
-    event_id_buy_iap_total = '5b7e764f3665cafb7b5fe340' # ok 
-    event_id_show_claim = '5b44647cdd26ca4c8f4db5ad'  # ok 
-    event_id_unlock_business= '5b44645e5a16bc136ebd03a1' # ok 
-    event_id_ad_play_action = '5b446ca85a16bc136ebd03a8' # ok 
-    event_id_ad_play_event = '5b63c9ce3665ca073aad10ff'  # ok 
+    event_id_buy_iap_total = '5b7e764f3665cafb7b5fe340'  # ok
+    event_id_show_claim = '5b44647cdd26ca4c8f4db5ad'  # ok
+    event_id_unlock_business = '5b44645e5a16bc136ebd03a1'  # ok
+    event_id_ad_play_action = '5b446ca85a16bc136ebd03a8'  # ok
+    event_id_ad_play_event = '5b63c9ce3665ca073aad10ff'  # ok
     event_id_ad_play_map2 = '5b7e51d20abec661afcec361'  # ok
-    event_id_ad_play_map3 = '5b9f5e589242950645e25132' # ok
+    event_id_ad_play_map3 = '5b9f5e589242950645e25132'  # ok
 
     g_product_id = id_tbc_android
     g_app_name = app_name_tbc3_android
@@ -150,22 +142,79 @@ def stat_platform_android():
     g_platform = platform_tbc3_android
 
 
-def query_active_users():
+def query_new_users():
+    """从umeng平台"""
     print(inspect.stack()[0][3])
-    try:
-        """查日活/新增"""
-        # https://mobile.umeng.com/apps/150000aed7d9a4f8eb9317b5/reports/load_table_data?start_date=2019-09-06&end_date=2019-09-14&versions[]=&channels[]=&segments[]=&time_unit=daily&stats=active
-        stats_type = 'active'
-        report_type = 'game_reports'
-        end_date = today.strftime('%Y-%m-%d')
-        start_date = (today - timedelta(days=interval)).strftime('%Y-%m-%d')
-        url = url_temlate % {"base_url": base_url, "id": g_product_id, 'report_type': report_type,
-                            'start_date': start_date, 'end_date': end_date, 'stats_type': stats_type}
-        resp = requests.get(url=url, cookies=cookies)
-        data = resp.json()
-        return {"昨天": [data['stats'][1]['active'], data['stats'][1]['install']], "前天": [data['stats'][2]['active'], data['stats'][2]['install']], '八天前': [data['stats'][-1]['active'], data['stats'][-1]['install']]}
-    except:
-        print("ERROR:query_active_users")
+    url = f"https://mobile.umeng.com/ht/api/v3/app/user/active/detail?relatedId={related_Id}"
+    end_date = (today - timedelta(days=0)).strftime('%Y-%m-%d')
+    start_date = (today - timedelta(days=7)).strftime('%Y-%m-%d')
+    # {"page":1,"pageSize":30,"fromDate":"2019-11-17","toDate":"2019-11-24","version":[],"channel":[],"timeUnit":"day","view":"newUser","relatedId":"5b7139be8f4a9d7dea000051"}
+    qdata_temlate = '{"page":1,"pageSize":30,"fromDate":"%(from_date)s","toDate":"%(to_date)s","version":[],"channel":[],"timeUnit":"day","view":"%(view)s","relatedId":"%(related_Id)s"}'
+    qdata_str = qdata_temlate % {
+        "from_date": start_date, "to_date": end_date, 'view':'newUser', 'related_Id': related_Id}
+    qdata = qdata_str.encode("utf-8")
+    query_type = "installation"
+    user_status_type = 'new'
+    headers = {
+        "authority": "mobile.umeng.com",
+        "method": "POST",
+        "path": f"/ht/api/v3/app/user/{user_status_type}/detail?relatedId={related_Id}",
+        "scheme": "https",
+        "accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh-TW;q=0.7,zh;q=0.6,ja;q=0.5",
+        "cache-control": "no-cache",
+        "Content-Type": "application/json;charset=UTF-8",
+        "cookie": f"{cookies_str.encode('utf-8')}",
+        "origin": "https://mobile.umeng.com",
+        "referer": f"https://mobile.umeng.com/platform/{related_Id}/reports/{query_type}",
+        "pragma": "no-cache",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
+    }
+
+    resp = requests.post(url=url, data=qdata, headers=headers)
+    data = resp.json()
+    return {"昨天": data['data']['items'][1]['newUser']}
+
+
+def query_active_users():
+    """从umeng平台"""
+    print(inspect.stack()[0][3])
+    url = f"https://mobile.umeng.com/ht/api/v3/app/user/active/detail?relatedId={related_Id}"
+    end_date = (today - timedelta(days=0)).strftime('%Y-%m-%d')
+    start_date = (today - timedelta(days=7)).strftime('%Y-%m-%d')
+    # {"page":1,"pageSize":30,"fromDate":"2019-11-17","toDate":"2019-11-24","version":[],"channel":[],"timeUnit":"day","view":"activeUserDay","relatedId":"5b7139be8f4a9d7dea000051"}
+    qdata_temlate = '{"page":1,"pageSize":30,"fromDate":"%(from_date)s","toDate":"%(to_date)s","version":[],"channel":[],"timeUnit":"day","view":"activeUserDay","relatedId":"%(related_Id)s"}'
+    qdata_str = qdata_temlate % {
+        "from_date": start_date, "to_date": end_date, 'related_Id': related_Id}
+    qdata = qdata_str.encode("utf-8")
+    query_type = "active_user"
+    user_status_type = 'active'
+    headers = {
+        "authority": "mobile.umeng.com",
+        "method": "POST",
+        "path": f"/ht/api/v3/app/user/{user_status_type}/detail?relatedId={related_Id}",
+        "scheme": "https",
+        "accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh-TW;q=0.7,zh;q=0.6,ja;q=0.5",
+        "cache-control": "no-cache",
+        "Content-Type": "application/json;charset=UTF-8",
+        "cookie": f"{cookies_str.encode('utf-8')}",
+        "origin": "https://mobile.umeng.com",
+        "referer": f"https://mobile.umeng.com/platform/{related_Id}/reports/{query_type}",
+        "pragma": "no-cache",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
+    }
+
+    resp = requests.post(url=url, data=qdata, headers=headers)
+    data = resp.json()
+    return {"昨天": data['data']['items'][1]['activeUser']}
+
 
 def query_launches_times():
     print(inspect.stack()[0][3])
@@ -263,7 +312,7 @@ def query_iap_users():
     stats_type = 'event_group_trend'
     report_type = 'events'
     event_group_id = event_group_id_buy_iap_total
-    
+
     end_date = (today - timedelta(days=1)).strftime('%Y-%m-%d')
     start_date = (today - timedelta(days=8)).strftime('%Y-%m-%d')
     url = url_event_temlate % {"base_url": base_url, "id": g_product_id, 'report_type': report_type,
@@ -512,10 +561,10 @@ def query_xi_back_gun():
         "Referrer Policy": "no-referrer-when-downgrade"
     }
 
-    resp = requests.post(url=url, data=qdata, headers=headers)
-    data = resp.json()
-
     try:
+        resp = requests.post(url=url, data=qdata, headers=headers)
+        data = resp.json()
+
         dict = {}
         for item in data[0]:
             if item["date"] == end_date and item["lifetime"] == 24:
@@ -559,11 +608,13 @@ def query_xi_ecpm(country=""):
         "x-token": "fangfang_ren"
     }
 
-    resp = requests.post(url=url, data=qdata_ecpm, headers=headers)
+    try:
+        resp = requests.post(url=url, data=qdata_ecpm, headers=headers)
+        data = resp.json()
 
-    data = resp.json()
-
-    return {"昨天": data[-1]['ecpm'], "前天": data[-2]['ecpm'], '八天前': data[1]['ecpm']}
+        return {"昨天": data[-1]['ecpm'], "前天": data[-2]['ecpm'], '八天前': data[1]['ecpm']}
+    except:
+        print("ERROR: query_xi_back_gun")
 
 
 # stat_platform_android()
@@ -573,3 +624,10 @@ def query_xi_ecpm(country=""):
 
 # stat_platform_android()
 # print(query_xi_back_gun())
+
+def test():
+    stat_platform_android()
+    print(query_new_users())
+
+
+test()
