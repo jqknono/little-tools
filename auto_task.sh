b8afcc1030aab1809e57ab99920d4e1f6982d352
jqknono <jqknono@outlook.com>
2020-01-01 14:18:17 +0800

    修改脚本

--- auto_task.sh
+++ auto_task.sh
@@ -1,5 +1,3 @@
 cd /home/fanfan/project
-
-/usr/bin/python3.6 /home/fanfan/project/main.py > task.txt 2>&1
-
-date >> task.txt
+date > task.txt
+/usr/bin/python3.6 /home/fanfan/project/main.py >> task.txt 2>&1
